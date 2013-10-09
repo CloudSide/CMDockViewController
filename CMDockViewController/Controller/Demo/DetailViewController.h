99e7fe5834e7cbae967b79ace6007b5986f6d8aa
Bruce Chen <662005@qq.com>
2013-10-09 14:11:26 +0800

    修改目录结构

---
+++ CMDockViewController/Controller/Demo/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  CMDockViewController
+//
+//  Created by hanchao on 13-10-8.
+//  Copyright (c) 2013年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UITableViewController
+
+@end
